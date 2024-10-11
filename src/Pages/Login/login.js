64f910a0c28d570f7a0ae47f956d39e2e613f110
Zafar563 <zafartohirov306@gmail.com>
2024-10-11 19:27:53 +0300

    loginpage

--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -1,10 +1,39 @@
 import React from 'react';
 
+
 function Login(props) {
     return (
-        <div className="Login">
-
+        
+        <div className='login-page'>
+        <div class="card">
+        <h2>Login Form</h2>
+        
+        
+        <div class="login_register">
+        <a href="#" class="login" target="blank">Login</a>
+        <a href="#" class="register" target="blank">Signup</a>
         </div>
+        
+        <form class="form">
+        <input type="email" placeholder="Email Adress" class="email"></input>
+        <input type="password" placeholder="password" class="pass"></input>
+        </form>
+        
+        
+        <a href="#" class="fp">Forgot password?</a>
+        
+        
+        <button type="button" class="login_btn">Login</button>
+        
+        
+        <div class="footer_card">
+        <p>Not a member?</p>
+        <a href="#">Singup now</a>
+        </div>
+        </div>
+        </div>
+        
+        
     );
 }
 
